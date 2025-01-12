8df6d5db1d62f642be3c5ea5409998138eb89b32
12112206 <106765873+12112206@users.noreply.github.com>
2025-01-12 18:57:19 +0800

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,44 @@
+f = open("record.txt")
+
+# this is person class
+class Person:
+  def __init__(self, name,company):
+    self.name = name
+    self.company = company
+    
+
+
+# this is company class
+class Company:
+  def __init__(self,name,product):
+    self.name = name
+    self.product = product
+    self.storage = {
+      self.product:0.0,
+      product.material:1.0
+      }
+    self.account = 0.0
+
+ 
+
+  def produce():pass
+  def deal(other,product,num):pass
+
+  
+
+
+
+
+
+
+
+# this is product class
+class Product:
+  def __init__(self,name,material,price):
+    self.name = name
+    self.material = material
+    self.cost = 1.1
+    self.price = price #price of one product
+    
+
+class Government (Company):pass
